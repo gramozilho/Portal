ca949c3f1556abe3a95c24cd60d1cbec67567380
gramozilho <miguelsemartins+1@gmail.com>
2020-01-26 20:47:50 +0000

    teleport with transform

--- Main2.gd
+++ Main2.gd
@@ -40,6 +40,8 @@ func _on_KinematicBody_new_portal(coord, orient, is_portal_A):
 		# Move portal A
 		portal_X = $MirrorA
 	
+	portal_X.orient = orient
+	print('or ', portal_X.orient)
 	portal_X.global_transform = Transform()
 	portal_X.global_transform.origin = coord + orient*.001
 	
@@ -64,16 +66,12 @@ func update_cam(main_cam_transform):
 	mirror_cam_B.global_transform = mirror_A.global_transform * mirror_B.global_transform.affine_inverse() * dummy_cam_B.global_transform
 	
 	# Position cameras
-	mirror_cam_A.global_transform.origin = mirror_B.global_transform.origin
-	mirror_cam_B.global_transform.origin = mirror_A.global_transform.origin
+	mirror_cam_A.global_transform.origin = mirror_B.global_transform.origin + mirror_B.orient*.1
+	mirror_cam_B.global_transform.origin = mirror_A.global_transform.origin + mirror_A.orient*.1
 	
 	mirror_cam_A.global_transform.basis.x *= -1
 	mirror_cam_B.global_transform.basis.x *= -1
 	
-	#mirror_cam_A.update_cull_mask(2)
-	#mirror_cam_B.update_cull_mask(3)
-	#mirror_A.global_transform = mirror_B.global_transform
-	#mirror_B.global_transform = mirror_A.global_transform
 
 func teleport(body, original_portal):
 	var target_portal
@@ -82,14 +80,42 @@ func teleport(body, original_portal):
 	else:
 		target_portal = $MirrorA
 	print('Teleport ' + body.name)
+	target_portal.start_timer()
+	var target_pos = target_portal.global_transform.origin + target_portal.orient*0.01
+	print('4 ', target_portal.rotation)
+	var target_orient = target_portal.rotation.normalized()
+	var origin_orient = original_portal.rotation.normalized()
 	
+	#$Player.global_transform.origin = target_pos + target_portal.orient*.1 # Vector3(0, 0, 0.5)#+ target_orient * 2
+	
+	# Align player
+	var old_rot = $Player/CameraAnchor.rotation.y
+	$Player.global_transform = target_portal.global_transform * original_portal.global_transform.affine_inverse()*$Player.global_transform
+	$Player/CameraAnchor.rotate_y(PI) #-target_orient.angle_to(origin_orient))
+	#$Player.look_at = $Player.global_transform.rotated(Vector3(0,1,0), PI/2)
+	#var old_angle = original_portal.orient.angle_to()
+	#$Player.look_at(target_portal.orient, Vector3(0,1,0))
+	#$Player.global_transform.origin = target_pos + target_portal.orient*.1 # Vector3(0, 0, 0.5)#+ target_orient * 2
+	
+
+func teleportB(body, original_portal):
+	var target_portal
+	if original_portal == $MirrorA:
+		target_portal = $MirrorB
+	else:
+		target_portal = $MirrorA
+	print('Teleport ' + body.name)
+	target_portal.start_timer()
 	var target_pos = target_portal.global_transform.origin 
 	print('4 ', target_portal.rotation)
 	var target_orient = target_portal.rotation.normalized()
-	$Player.global_transform.origin = target_pos + Vector3(0, 0, 0.5)#+ target_orient * 2
+	$Player.global_transform.origin = target_pos + target_portal.orient*.1 # Vector3(0, 0, 0.5)#+ target_orient * 2
 	
+	# Align player
+	var old_angle = original_portal.orient.angle_to()
+	$Player.look_at(target_portal.orient, Vector3())
 	# Rotate
-	var dir_rot = $Player/CameraAnchor.rotation.y + 120 # keep original rot
+	var dir_rot = $Player/CameraAnchor.rotation.y# + 120 # keep original rot
 	dir_rot += -(1-original_portal.rotation.normalized().dot(target_portal.rotation.normalized()))*90 # account for different portal angles
 	print('a: ', original_portal.rotation.normalized().dot(target_portal.rotation.normalized()))
 	$Player/CameraAnchor.rotate_y(deg2rad(dir_rot))
--- Mirror2.gd
+++ Mirror2.gd
@@ -2,12 +2,16 @@ extends MeshInstance
 
 signal teleport
 export (NodePath) var viewport_mirror
+var orient = Vector3()
 
 func _ready():
 	pass #self.get_surface_material(0).set_shader_param("refl_tx", viewport_mirror)
 
 
 func _on_Area_body_entered(body):
-	if body.is_in_group('teleport'):
+	if body.is_in_group('teleport') and $Timer.is_stopped():
 		print(self.name)
 		emit_signal('teleport', body, self)
+
+func start_timer():
+	$Timer.start()
